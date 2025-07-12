6b3f1caac654797c35377266ff7c8484152b0534
Tai Tran <tranhuutai13011@gmail.com>
2025-07-12 20:46:53 +0700

    Mô tả thay đổi

--- 3_SQL/SQLQuery_P1.sql
+++ 3_SQL/SQLQuery_P1.sql
@@ -1,19 +1,30 @@
-SELECT * FROM taibd..life_expectancy
-
-EXEC taibd..life_expectancy
--- Data exploration:
-SELECT * FROM taibd..life_expectancy;
-SELECT TOP 100 * FROM taibd..life_expectancy;
-SELECT COUNT(*) FROM taibd..life_expectancy;
--- NULL: 
-SELECT COUNT(*) AS number_null_of_Country
-FROM taibd..life_expectancy
-WHERE Country IS NULL
-
-SELECT COUNT(*) AS number_null_of_Year
-FROM taibd..life_expectancy
-WHERE Year IS NULL
-
-SELECT COUNT(*) AS number_null_of_Country
-FROM taibd..life_expectancy
-WHERE Status iS NULL
+SELECT * FROM Munich03
+-- Some lines have yearc as a decimal number
+SELECT yearc
+FROM Munich03
+WHERE yearc IS NOT NULL AND yearc <> FLOOR(yearc);
+-- Overwrite yearc with its integer (floor) value
+UPDATE Munich03
+SET yearc = FLOOR(yearc)
+WHERE yearc IS NOT NULL;
+-- No more
+SELECT yearc
+FROM Munich03
+WHERE yearc IS NOT NULL AND yearc <> FLOOR(yearc);
+-- See NULL
+SELECT COUNT(*)
+FROM Munich03
+WHERE 
+    rent IS NULL OR
+    rentsqm IS NULL OR
+    area IS NULL OR
+    rooms IS NULL OR
+    yearc IS NULL OR
+    bathextra IS NULL OR
+    bathtile IS NULL OR
+    cheating IS NULL OR
+    district IS NULL OR
+    location IS NULL OR
+    upkitchen IS NULL OR
+    wwater IS NULL
+-- No NULL
